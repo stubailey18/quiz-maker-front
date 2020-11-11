9747823c7531b4d82a714b20d981a648b34ed020
Stuart Bailey <stubailey18@hotmail.com>
2020-11-11 22:35:39 +0000

    Changed the Question component so that it no longer manipulates the DOM directly

--- src/components/Question.js
+++ src/components/Question.js
@@ -10,18 +10,53 @@ const shuffleArray = array => {
     return [...array];
 }
 
-const toHtmlId = str => {
-    return `_${str.replaceAll(' ', '_')}`;
-}
-
 export default function Question(props) {
+
     const {question, correctAnswer, distractors, imageUrl} = props.question;
     const {onCorrectAnswer, onNextQuestion} = props;
+
     const [answers, setAnswers] = useState([]);
-    const [answerButtonsDisabled, setAnswerButtonsDisabled] = useState(false);
+    const [questionAnswered, setQuestionAnswered] = useState(false);
+    const [userAnswer, setUserAnswer] = useState(null);
+
     useEffect(() => {
         setAnswers(shuffleArray([...distractors, correctAnswer]));
     }, [correctAnswer, distractors]);
+
+    const handleAnswerChange = event => {
+        setQuestionAnswered(true);
+        setUserAnswer(event.target.value);
+        if (event.target.value === correctAnswer) {
+            onCorrectAnswer();
+        }
+    }
+
+    const getInputContainerClasses = inputValue => {
+        if (questionAnswered) {
+            // if the question has been answered and the input value matches the correct answer
+            // then mark this answer as the right one regardless of whether it's the one the user selected
+            if (inputValue === correctAnswer) {
+                return 'rightAnswer';
+            // the question has been answered (and this input value does NOT match the correct answer)
+            // but it does match the user answer then mark this answer as the wrong one (as selected by the user)
+            } else if (inputValue === userAnswer) {
+                return 'wrongAnswer';
+            // return an empty string to avoid an 'undefined' class being added
+            } else {
+                return '';
+            }
+        // return an empty string to avoid an 'undefined' class being added
+        } else {
+            return '';
+        }
+    }
+
+    const handleNextButtonClick = event => {
+        setQuestionAnswered(false);
+        setUserAnswer(null);
+        onNextQuestion();
+    }
+
     return (
         <div className="row justify-content-center">
             <div className="container col-sm-10 col-lg-8">
@@ -29,36 +64,23 @@ export default function Question(props) {
                     <img alt={question} src={imageUrl} className="card-img-top" />
                     <div className="card-body">
                         <h5 className="card-title">{question}</h5>
-                        {answers.map(answer => (
-                            <div id={`${toHtmlId(answer)}`} key={answer} className="form-check">
+                        {answers.map((answer, index) => (
+                            <div key={index} className={`form-check ${getInputContainerClasses(answer)}`}>
                                 <label className="form-check-label">
                                     <input
                                         type="radio"
                                         name="answer"
                                         value={answer}
-                                        onChange={() => {
-                                            if (answer === correctAnswer) {
-                                                document.querySelector(`#${toHtmlId(answer)}`).classList.add('rightAnswer');
-                                                onCorrectAnswer();
-                                            } else {
-                                                document.querySelector(`#${toHtmlId(correctAnswer)}`).classList.add('rightAnswer');
-                                                document.querySelector(`#${toHtmlId(answer)}`).classList.add('wrongAnswer');
-                                            }
-                                            setAnswerButtonsDisabled(true);
-                                        }}
-                                        disabled={answerButtonsDisabled}
+                                        onChange={handleAnswerChange}
+                                        checked={questionAnswered}
+                                        disabled={questionAnswered}
                                         className="form-check-input d-block">
                                     </input>{answer}
                                 </label>
                             </div>
                         ))}
                         <button 
-                            onClick={() => {
-                                setAnswerButtonsDisabled(false);
-                                document.getElementsByName('answer').forEach(e => e.checked = false);
-                                document.querySelectorAll('div').forEach(e => e.classList.remove('rightAnswer', 'wrongAnswer'));
-                                onNextQuestion();
-                            }} className="btn btn-dark mt-3">
+                            onClick={handleNextButtonClick} className="btn btn-dark mt-3">
                             Next
                         </button>
                     </div>
